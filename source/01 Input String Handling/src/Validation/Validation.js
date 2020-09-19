cc68bb092c59d9639a4f31a3bb91a8fe8fa8b02f
04wakeup <dojun849@gmail.com>
2020-09-19 11:17:03 -0700

    init commit

---
+++ source/01 Input String Handling/src/Validation/Validation.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+import './Validation.css';
+
+const validation = ( props ) => {
+    return (
+        <div className="Validation"> 
+        </div>
+    )
+};
+
+export default validation;
