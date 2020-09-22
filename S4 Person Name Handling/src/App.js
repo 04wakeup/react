76ecf1f6fb4d7fe55d3d98392d377723bbd82324
04wakeup <dojun849@gmail.com>
2020-09-21 18:07:19 -0700

    70 intro styled comp

--- S4 Person Name Handling/src/App.js
+++ S4 Person Name Handling/src/App.js
@@ -1,7 +1,6 @@
 import React, { Component } from 'react';
 import './App.css';
-import Person from './Person/Person';
-import Radium, { StyleRoot } from 'radium';
+import Person from './Person/Person'; 
 
 class App extends Component {
   state = {
@@ -90,8 +89,7 @@ class App extends Component {
       classes.push('bold');  // red bold
     }
 
-    return (
-      <StyleRoot>
+    return ( 
       <div className="App">
         <h1>Hi, I'm a React App</h1>
         <p className={classes.join(' ')}>This is really working!</p>
@@ -99,11 +97,10 @@ class App extends Component {
           style={style}
           onClick={(event) => this.togglePersonsHandler(event,) }>Toggle Persons</button>
         {persons}
-      </div>
-      </StyleRoot>
+      </div> 
     );
     // return React.createElement('div', {className: 'App'}, React.createElement('h1', null, 'Does this work now?'));
   }
 }
 
-export default Radium(App);
+export default  App;
