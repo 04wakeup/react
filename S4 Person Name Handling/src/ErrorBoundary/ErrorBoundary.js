87dcc0984483811e9b4c1723b8a76200bfa549b1
04wakeup <dojun849@gmail.com>
2020-09-23 13:28:20 -0700

    before s7 deep Component and React internal

---
+++ S4 Person Name Handling/src/ErrorBoundary/ErrorBoundary.js
@@ -0,0 +1,21 @@
+import React, { Component } from 'react';
+
+class ErrorBoundary extends Component{
+    state = {
+        hasError: false,
+        errorMesssage: ''
+    }
+    componentDidCatch = (error, info) => {
+        this.setState({hasError: true, errorMesssage: error});
+    }
+    render() {
+        if(this.state.hasError){
+            return <h1>{this.state.errorMesssage}</h1>;
+        } else {
+            return this.props.children;
+        }
+        
+    }
+}
+
+export default ErrorBoundary;
