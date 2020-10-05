77fe33e50eea04e02bb6ac615b6651fc18738659
04wakeup <dojun849@gmail.com>
2020-10-05 11:14:36 -0700

    Person done

--- S4 Person Name Handling/src/components/Persons/Person/Person.js
+++ S4 Person Name Handling/src/components/Persons/Person/Person.js
@@ -1,24 +1,52 @@
 import React, { Component } from 'react'; 
+import PropTypes from 'prop-types';
 // import styled from 'styled-components'; 
 
 import classes from './Person.css';
+import withClass from '../../../hoc/withClass'
 import Aux from '../../../hoc/Auxiliary'
+import AuthContext from '../../../context/auth-context';
 
 class Person extends Component{  
+    constructor(props){
+        super(props);
+        this.inputElementRef = React.createRef();
+    } 
+
+    static contextType = AuthContext;
+
+
+    componentDidMount(){
+        // this.inputElement.focus();
+        this.inputElementRef.current.focus();
+        console.log(this.context.authenticated);
+    }
+
     render(){
     console.log('[Person.js] rendering...'); 
-        return (
-            <Aux>
-                <p  onClick={this.props.click}>I'm {this.props.name} and I am {this.props.age} years old!</p>
-                <p >{this.props.children}</p>
-                <input 
-                    type="text" 
-                    onChange={this.props.changed} 
-                    value={this.props.name} /> 
+        return ( 
+            <Aux> 
+                    { this.context.authenticated ? <p>Authenticated!</p> : <p>Please log in</p>} 
+                    
+                    <p  onClick={this.props.click}>I'm {this.props.name} and I am {this.props.age} years old!</p>
+                    <p key="i2">{this.props.children}</p>
+                    <input key="i3"
+                        // ref={(inputEl)=>{this.inputElement = inputEl}}
+                        ref={this.inputElementRef}
+                        type="text" 
+                        onChange={this.props.changed} 
+                        value={this.props.name} /> 
+                
             </Aux>
         );
     }
     
 };
+Person.propTypes = {
+    click: PropTypes.func,
+    name: PropTypes.string,
+    age: PropTypes.number,
+    changed: PropTypes.func
+};
 
-export default  Person;
+export default  withClass(Person, classes.Person);
