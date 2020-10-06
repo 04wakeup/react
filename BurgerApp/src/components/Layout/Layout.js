bd63b290cb3487ea8f4da2b07791036c8480cc19
04wakeup <dojun849@gmail.com>
2020-10-06 14:01:12 -0700

    adding ingredients

--- BurgerApp/src/components/Layout/Layout.js
+++ BurgerApp/src/components/Layout/Layout.js
@@ -6,7 +6,7 @@ const layout = (props) => (
     <Auxiliary>
         <div>Toolbar, SideDrawer, Backdrop</div>
         <main className={classes.Content}>
-            {props.children}
+            {props.children}  
         </main>
     </Auxiliary>
     
