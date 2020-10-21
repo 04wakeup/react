37f359053b7e9632eff23f37bb99ec9014ef1a77
04wakeup <dojun849@gmail.com>
2020-10-21 15:32:44 -0700

    complete http test

--- http-ajax/src/containers/Blog/Blog.js
+++ http-ajax/src/containers/Blog/Blog.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import axios from 'axios';
+import axios from '../../axios';
 import Post from '../../components/Post/Post';
 import FullPost from '../../components/FullPost/FullPost';
 import NewPost from '../../components/NewPost/NewPost';
@@ -7,12 +7,14 @@ import './Blog.css';
 
 class Blog extends Component {
     state = {
-        posts : []
+        posts : [],
+        selectedPostId: null,
+        error: false
     }
     componentDidMount(){
-        axios.get('http://jsonplaceholder.typicode.com/posts')
+        axios.get('/posts')
             .then(response => {
-                const posts = response.data.slice(0, 400);
+                const posts = response.data.slice(0, 4);
                 const updatedPosts = posts.map(post => {
                     return {
                         ...post,
@@ -20,12 +22,29 @@ class Blog extends Component {
                     }
                 });
                 this.setState({posts: updatedPosts}); 
+            })
+            .catch(error => {
+                // console.log(error);
+                this.setState({error: true})
             });
     }
+    
+    postSelectedHandler = (id) => {
+        this.setState({selectedPostId: id});
+    }
     render () {
-        const posts = this.state.posts.map(post => {
-            return <Post key={post.id} title={post.title} author={post.author}/>;
-        })
+        let posts = <p style={{textAlign: 'center'}}>Something is wrong</p>; 
+        if (!this.state.error){
+            console.log('hey');
+              posts = this.state.posts.map(post => {
+                return <Post 
+                            key={post.id} 
+                            title={post.title} 
+                            author={post.author}
+                            clicked={() => this.postSelectedHandler(post.id)}/>;
+            })
+        }
+      
         // const posts = this.state.posts.map(post => {
         //     return <Post title={post.tilte} />
         // });
@@ -35,7 +54,7 @@ class Blog extends Component {
                     {posts}
                 </section>
                 <section>
-                    <FullPost />
+                    <FullPost id={this.state.selectedPostId}/>
                 </section>
                 <section>
                     <NewPost />
